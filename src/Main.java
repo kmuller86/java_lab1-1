980962e23bf7d996db965ffbd19ca838c8018c28
pc <offiner@gmail.com>
2021-09-20 18:02:44 +0200

    ćwiczenie 1 zrobione

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,15 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
+        System.out.println("Cześć!");
+        System.out.println("Dopiero uczę się tego języka");
+        System.out.println("Wolałbym" + " " + "dalej " + "pisać w c#!");
 
+        System.out.printf("Ale %s jest taki %s program.\n", "tutaj", "akurat");
+
+        System.out.print("Ala ma kota");
+        System.out.print(" ,a");
+        System.out.print(" kot ma alę.");
 
     }
 }
