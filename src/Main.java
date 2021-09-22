49975e24d5c10e7a12cb9b0257f7c978678ffe34
pc <offiner@gmail.com>
2021-09-22 17:52:19 +0200

    ćwiczenie 6

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,15 @@ public class Main {
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
 
+        double[] tablica = new double[5];
+        tablica[0] = 1.4;
+        tablica[1] = 3.1;
+        tablica[2] = 5.4;
+        tablica[3] = 3.5;
+        tablica[4] = 2.4;
+
+        for (int i = 0; i < 5; i++)
+            System.out.println("Kolejna komórka to: " + tablica[i]);
+
     }
 }
